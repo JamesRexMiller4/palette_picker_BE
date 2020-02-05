24c6166a71e03fcfbc7dec1eae1762e454523c42
sertmer <scottertmer@gmail.com>
2020-02-04 17:46:03 -0700

    Create test seed file

---
+++ testData.js
@@ -0,0 +1,38 @@
+const testUsersData = [
+  {username: 'Jarvis Blargus', password: 'password123', folders: [
+    {folderName: 'Project #1', palettes: [
+      {paletteName: 'Palette #1', colors: ['#80adaa', '#d9c9fb', '#9c5f3d', '#4ba9b3', '#a6617c']},
+      {paletteName: 'Palette #2', colors: ['#80e56a', '#51c5c5', '#51ed3d', '#7f6851', '#826fb0']},
+      {paletteName: 'Palette #3', colors: ['#80e56a', '#51c5c5', '#51ed3d', '#7f6851', '#826fb0']}
+    ]},
+    {folderName: 'Project #2', palettes: [
+      {paletteName: 'Palette #1', colors: ['#80e56a', '#51c5c5', '#51ed3d', '#7f6851', '#826fb0']},
+      {paletteName: 'Palette #2', colors: ['#80adaa', '#d9c9fb', '#9c5f3d', '#4ba9b3', '#a6617c']}, 
+      {paletteName: 'Palette #3', colors: ['#db88e5', '#f09231', '#e1c902', '#9b8603', '#160c54']},
+    ]},
+    {folderName: 'Project #3', palettes: [
+      {paletteName: 'Palette #1', colors: ['#6ce8c8', '#b8a7d1', '#258fd5', '#ceb025', '#b09d5e']},
+      {paletteName: 'Palette #2', colors: ['#47e18d', '#75880e', '#3560fe', '#989a53', '#5e5ed4']},
+      {paletteName: 'Palette #3', colors: ['#0441af', '#f5933e', '#0318e1', '#576f1d', '#0d3c03']},
+    ]}]
+  },
+  {username: 'Banksy', password: 'password123', folders: [
+    {folderName: 'Project #1', palettes: [ 
+      {paletteName: 'Palette #1', colors: ['#c90629', '#30d60f', '#a18dc1', '#8ea780', '#43c877']},
+      {paletteName: 'Palette #2', colors: ['#c0d1ea', '#933016', '#35d018', '#f87f3f', '#b9481e']},
+      {paletteName: 'Palette #3', colors: ['#3b29f5', '#ce73e5', '#df70b6', '#e39cbf', '#f6b26d']},
+    ]},
+    {folderName: 'Project #2', palettes: [
+      {paletteName: 'Palette #1', colors: ['#3b29f5', '#ce73e5', '#df70b6', '#e39cbf', '#f6b26d']},
+      {paletteName: 'Palette #2', colors: ['#db88e5', '#f09231', '#e1c902', '#9b8603', '#160c54']},
+      {paletteName: 'Palette #3', colors: ['#80adaa', '#d9c9fb', '#9c5f3d', '#4ba9b3', '#a6617c']},
+    ]},
+    {folderName: 'Project #3', palettes: [
+      {paletteName: 'Palette #1', colors: ['#80e56a', '#51c5c5', '#51ed3d', '#7f6851', '#826fb0']},
+      {paletteName: 'Palette #2', colors: ['#47e18d', '#75880e', '#3560fe', '#989a53', '#5e5ed4']},
+      {paletteName: 'Palette #3', colors: ['#6ce8c8', '#b8a7d1', '#258fd5', '#ceb025', '#b09d5e']},
+    ]}]
+  }
+]
+
+module.exports = testUsersData;
