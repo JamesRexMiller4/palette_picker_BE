eea724a6a7e467193bd722b52d49d6e8e4e78203
sertmer <scottertmer@gmail.com>
2020-02-04 17:06:07 -0700

    Build initial server.js file

---
+++ app.test.js
@@ -0,0 +1,6 @@
+import request from 'supertest'
+import app from './app'
+
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
