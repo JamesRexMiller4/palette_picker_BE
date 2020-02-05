2861a7d82457ed12b6be70b0ea1097332cd3e229
sertmer <scottertmer@gmail.com>
2020-02-04 19:44:01 -0700

    Add GET all folders from a user endpoint

--- app.js
+++ app.js
@@ -14,4 +14,17 @@ app.get('/', (request, response) => {
   response.send('Welcome to the Palette Picker API');
 });
 
+app.get('/api/v1/users/:id/folders', async (request, response) => {
+  const { id } = request.params
+
+  try {
+    const folders = await database('folders')
+      .where('id', id)
+      .select();
+    response.status(200).json(folders)
+  } catch(error) {
+    response.status(500).json({error})
+  }
+
+})
 module.exports = app;
