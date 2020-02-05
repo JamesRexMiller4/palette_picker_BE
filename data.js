9c88abd65db61bcf9f397fd770dc07d3d7982d77
sertmer <scottertmer@gmail.com>
2020-02-05 16:18:23 -0700

    Change format of data and test data to reflect no users

--- data.js
+++ data.js
@@ -1,5 +1,4 @@
 const data = [
-  {username: 'Jarvis Blargus', password: 'password123', folders: [
     {folderName: 'Project #1', palettes: [
       {paletteName: 'Palette #1', colors: ['#80adaa', '#d9c9fb', '#9c5f3d', '#4ba9b3', '#a6617c']},
       {paletteName: 'Palette #2', colors: ['#80e56a', '#51c5c5', '#51ed3d', '#7f6851', '#826fb0']},
@@ -14,42 +13,37 @@ const data = [
       {paletteName: 'Palette #1', colors: ['#6ce8c8', '#b8a7d1', '#258fd5', '#ceb025', '#b09d5e']},
       {paletteName: 'Palette #2', colors: ['#47e18d', '#75880e', '#3560fe', '#989a53', '#5e5ed4']},
       {paletteName: 'Palette #3', colors: ['#0441af', '#f5933e', '#0318e1', '#576f1d', '#0d3c03']},
-    ]}]
-  },
-  {username: 'Banksy', password: 'password123', folders: [
-    {folderName: 'Project #1', palettes: [ 
+    ]},
+    {folderName: 'Project #4', palettes: [ 
       {paletteName: 'Palette #1', colors: ['#c90629', '#30d60f', '#a18dc1', '#8ea780', '#43c877']},
       {paletteName: 'Palette #2', colors: ['#c0d1ea', '#933016', '#35d018', '#f87f3f', '#b9481e']},
       {paletteName: 'Palette #3', colors: ['#3b29f5', '#ce73e5', '#df70b6', '#e39cbf', '#f6b26d']},
     ]},
-    {folderName: 'Project #2', palettes: [
+    {folderName: 'Project #5', palettes: [
       {paletteName: 'Palette #1', colors: ['#3b29f5', '#ce73e5', '#df70b6', '#e39cbf', '#f6b26d']},
       {paletteName: 'Palette #2', colors: ['#db88e5', '#f09231', '#e1c902', '#9b8603', '#160c54']},
       {paletteName: 'Palette #3', colors: ['#80adaa', '#d9c9fb', '#9c5f3d', '#4ba9b3', '#a6617c']},
     ]},
-    {folderName: 'Project #3', palettes: [
+    {folderName: 'Project #6', palettes: [
       {paletteName: 'Palette #1', colors: ['#80e56a', '#51c5c5', '#51ed3d', '#7f6851', '#826fb0']},
       {paletteName: 'Palette #2', colors: ['#47e18d', '#75880e', '#3560fe', '#989a53', '#5e5ed4']},
       {paletteName: 'Palette #3', colors: ['#6ce8c8', '#b8a7d1', '#258fd5', '#ceb025', '#b09d5e']},
-    ]}]
-  },
-  {username: 'Scott Ertmer', password: 'password123', folders: [
-    {folderName: 'Project #1', palettes: [
+    ]},
+    {folderName: 'Project #7', palettes: [
       {paletteName: 'Palette #1', colors: ['#0441af', '#f5933e', '#0318e1', '#576f1d', '#0d3c03']},
       {paletteName: 'Palette #2', colors: ['#c90629', '#30d60f', '#a18dc1', '#8ea780', '#43c877']},
       {paletteName: 'Palette #3', colors: ['#c0d1ea', '#933016', '#35d018', '#f87f3f', '#b9481e']},
     ]},
-    {folderName: 'Project #2', palettes: [
+    {folderName: 'Project #8', palettes: [
       {paletteName: 'Palette #1', colors: ['#3b29f5', '#ce73e5', '#df70b6', '#e39cbf', '#f6b26d']},
       {paletteName: 'Palette #2', colors: ['#db88e5', '#f09231', '#e1c902', '#9b8603', '#160c54']},
       {paletteName: 'Palette #3', colors: ['#80adaa', '#d9c9fb', '#9c5f3d', '#4ba9b3', '#a6617c']},
     ]},
-    {folderName: 'Project #3', palettes: [
+    {folderName: 'Project #9', palettes: [
       {paletteName: 'Palette #1', colors: ['#80e56a', '#51c5c5', '#51ed3d', '#7f6851', '#826fb0']},
       {paletteName: 'Palette #1', colors: ['#6ce8c8', '#b8a7d1', '#258fd5', '#ceb025', '#b09d5e']},
       {paletteName: 'Palette #1', colors: ['#47e18d', '#75880e', '#3560fe', '#989a53', '#5e5ed4']},
-    ]}]
-  }
+    ]}
 ]
 
 module.exports = data;
